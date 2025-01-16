fedb0fc034a5c964065bc62c5fcc0ecc5b1def40
Sachin Poudel <poudelsachin009@gmail.com>
2025-01-16 16:41:55 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+//         let spinnerr = document.querySelector('.spinner'); 
+
+// $(window).on("load", function () {
+//     $(".spinnerr").fadeOut(); // Fade out loader
+// });
+
+// let menuu = document.querySelector('#menuu-icon');
+
+// menuu.onclick = () => {
+//     navbar.style.right = "-200px"
+
+// }
+   // back function code
+// const hamburger = document.getElementById('hamburger');
+// const menu = document.getElementById('menu');
+
+// let isMenuOpen = false; // Track the menu state
+
+// Open and Close Menu
+// menu.addEventListener('click', () => {
+//     if (isMenuOpen) {
+//         closeMenu();
+//     } else {
+//         openMenu();
+//     }
+// });
+
+// // Open Menu Function
+// function openMenu() {
+//     navbar.style.display = 'flex';
+//     isMenuOpen = true;
+
+//     // Push a new state to history
+//     history.pushState({ menuOpen: true }, null, '');
+// }
+
+// // Close Menu Function
+// function closeMenu() {
+//     navbar.style.display = 'none';
+//     isMenuOpen = false;
+
+//     // Go back in history
+//     if (window.history.state && window.history.state.menuOpen) {
+//         history.back();
+//     }
+// }
+
+// // Listen for the back button (popstate event)
+// window.addEventListener('popstate', (event) => {
+//     if (isMenuOpen && (!event.state || !event.state.menuOpen)) {
+//         closeMenu();
+//     }
+// });
